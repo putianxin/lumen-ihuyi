1499bb25279b074bb286757879988598fc3d4e58
putianxin@shanlaan <382820748@qq.com>
2018-08-27 18:36:49 +0800

    config changes

--- config/ihuyi.php
+++ config/ihuyi.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-    'ihuyi' => [
-        // 互亿无线分配的 APPID
-        'APIID' => env('HUYI_APP_ID', ''),
-        // 互亿无线异步通知地址
-        'APIKEY' => env('HUYI_API_KEY', ''),
-    ],
+   // 互亿无线分配的 APPID
+   'APIID' => env('HUYI_APP_ID', ''),
+   // 互亿无线异步通知地址
+   'APIKEY' => env('HUYI_API_KEY', ''),
 ];
